8414801caf3787ed645ac3bf0056ed8536547cdc
brlamit <itsmeamit991@gmail.com>
2023-12-19 21:11:45 +0545

    Add project files.

---
+++ junk MS/home.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class home
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GunaGradient2Panel1 = New Guna.UI.WinForms.GunaGradient2Panel()
+        Me.GunaAnimateWindow1 = New Guna.UI.WinForms.GunaAnimateWindow(Me.components)
+        Me.SuspendLayout()
+        '
+        'GunaGradient2Panel1
+        '
+        Me.GunaGradient2Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.GunaGradient2Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GunaGradient2Panel1.GradientColor1 = System.Drawing.Color.DarkTurquoise
+        Me.GunaGradient2Panel1.GradientColor2 = System.Drawing.Color.SteelBlue
+        Me.GunaGradient2Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.GunaGradient2Panel1.Name = "GunaGradient2Panel1"
+        Me.GunaGradient2Panel1.Size = New System.Drawing.Size(1504, 832)
+        Me.GunaGradient2Panel1.TabIndex = 0
+        '
+        'GunaAnimateWindow1
+        '
+        Me.GunaAnimateWindow1.AnimationType = Guna.UI.WinForms.GunaAnimateWindow.AnimateWindowType.AW_HOR_NEGATIVE
+        Me.GunaAnimateWindow1.Interval = 100
+        Me.GunaAnimateWindow1.TargetControl = Nothing
+        '
+        'home
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1504, 832)
+        Me.Controls.Add(Me.GunaGradient2Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "home"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "home"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GunaGradient2Panel1 As Guna.UI.WinForms.GunaGradient2Panel
+    Friend WithEvents GunaAnimateWindow1 As Guna.UI.WinForms.GunaAnimateWindow
+End Class
