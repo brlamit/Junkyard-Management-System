5e37e7dba32278faa57d6ae20df233ef45aa71bb
Sangam subedi <subedisangam183@gmail.com>
2023-12-21 13:35:13 +0530

    new home page design

--- junk MS/home.vb
+++ junk MS/home.vb
@@ -1,11 +1,11 @@
 ﻿Imports Guna.UI.WinForms
 
 Public Class home
-    Private Sub GunaGradient2Panel1_Paint(sender As Object, e As PaintEventArgs) Handles GunaGradient2Panel1.Paint
+    Private Sub GunaGradient2Panel1_Paint(sender As Object, e As PaintEventArgs)
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BtnMenu.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
 
     End Sub
     Dim sidebar As String = "close "
@@ -33,11 +33,17 @@ Public Class home
 
     End Sub
 
-    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs)
         Application.Exit()
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
         Me.Dispose()
     End Sub
+
+    Private Sub TabControl1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+
 End Class
